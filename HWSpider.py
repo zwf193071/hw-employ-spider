d63be9faeab5df56fc91087a826325d6eb7e523e
01403558 <zhuwenfang@sfmail.sf-express.com>
2021-07-12 19:31:41 +0800

    Using python to capture datas, then export the datas into excel

---
+++ HWSpider.py
@@ -0,0 +1,189 @@
+"""ua大列表"""
+USER_AGENT_LIST = [
+                  'Mozilla/5.0 (Windows NT 6.2; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.90 Safari/537.36',
+                  'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3451.0 Safari/537.36',
+                  'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.9; rv:57.0) Gecko/20100101 Firefox/57.0',
+                  'Mozilla/5.0 (Windows NT 5.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/28.0.1500.71 Safari/537.36',
+                  'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.2999.0 Safari/537.36',
+                  'Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.70 Safari/537.36',
+                  'Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10.4; en-US; rv:1.9.2.2) Gecko/20100316 Firefox/3.6.2',
+                  'Mozilla/5.0 (Windows NT 5.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.155 Safari/537.36 OPR/31.0.1889.174',
+                  'Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 6.1; Trident/4.0; SLCC2; .NET CLR 2.0.50727; .NET CLR 3.5.30729; .NET CLR 3.0.30729; .NET CLR 1.1.4322; MS-RTC LM 8; InfoPath.2; Tablet PC 2.0)',
+                  'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.100 Safari/537.36',
+                  'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/68.0.3440.106 Safari/537.36 OPR/55.0.2994.61',
+                  'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/535.1 (KHTML, like Gecko) Chrome/14.0.814.0 Safari/535.1',
+                  'Mozilla/5.0 (Macintosh; U; PPC Mac OS X; ja-jp) AppleWebKit/418.9.1 (KHTML, like Gecko) Safari/419.3',
+                  'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/43.0.2357.134 Safari/537.36',
+                  'Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 6.1; Trident/6.0; Touch; MASMJS)',
+                  'Mozilla/5.0 (X11; Linux i686) AppleWebKit/535.21 (KHTML, like Gecko) Chrome/19.0.1041.0 Safari/535.21',
+                  'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.100 Safari/537.36',
+                  'Mozilla/5.0 (Windows NT 6.2; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.90 Safari/537.36',
+                  'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3451.0 Safari/537.36',
+                  'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.9; rv:57.0) Gecko/20100101 Firefox/57.0',
+                  'Mozilla/5.0 (Windows NT 5.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/28.0.1500.71 Safari/537.36',
+                  'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.2999.0 Safari/537.36',
+                  'Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.70 Safari/537.36',
+                  'Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10.4; en-US; rv:1.9.2.2) Gecko/20100316 Firefox/3.6.2',
+                  'Mozilla/5.0 (Windows NT 5.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.155 Safari/537.36 OPR/31.0.1889.174',
+                  'Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 6.1; Trident/4.0; SLCC2; .NET CLR 2.0.50727; .NET CLR 3.5.30729; .NET CLR 3.0.30729; .NET CLR 1.1.4322; MS-RTC LM 8; InfoPath.2; Tablet PC 2.0)',
+                  'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.100 Safari/537.36',
+                  'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/68.0.3440.106 Safari/537.36 OPR/55.0.2994.61',
+                  'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/535.1 (KHTML, like Gecko) Chrome/14.0.814.0 Safari/535.1',
+                  'Mozilla/5.0 (Macintosh; U; PPC Mac OS X; ja-jp) AppleWebKit/418.9.1 (KHTML, like Gecko) Safari/419.3',
+                  'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/43.0.2357.134 Safari/537.36',
+                  'Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 6.1; Trident/6.0; Touch; MASMJS)',
+                  'Mozilla/5.0 (X11; Linux i686) AppleWebKit/535.21 (KHTML, like Gecko) Chrome/19.0.1041.0 Safari/535.21',
+                  'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.100 Safari/537.36',
+                  'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4093.3 Safari/537.36',
+                  'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_5) AppleWebKit/537.36 (KHTML, like Gecko; compatible; Swurl) Chrome/77.0.3865.120 Safari/537.36',
+                  'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36',
+                  'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36',
+                  'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.131 Safari/537.36',
+                  'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4086.0 Safari/537.36',
+                  'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:75.0) Gecko/20100101 Firefox/75.0',
+                  'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) coc_coc_browser/91.0.146 Chrome/85.0.4183.146 Safari/537.36',
+                  'Mozilla/5.0 (Windows; U; Windows NT 5.2; en-US) AppleWebKit/537.36 (KHTML, like Gecko) Safari/537.36 VivoBrowser/8.4.72.0 Chrome/62.0.3202.84',
+                  'Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.101 Safari/537.36',
+                  'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36 Edg/87.0.664.60',
+                  'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.16; rv:83.0) Gecko/20100101 Firefox/83.0',
+                  'Mozilla/5.0 (X11; CrOS x86_64 13505.63.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36',
+                  'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.9; rv:68.0) Gecko/20100101 Firefox/68.0',
+                  'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.101 Safari/537.36',
+                  'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36',
+                  'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.198 Safari/537.36 OPR/72.0.3815.400',
+                  'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.101 Safari/537.36',
+                  ]
+from requests_html import HTMLSession
+import os, xlwt, xlrd, random
+from xlutils.copy import copy
+session = HTMLSession()
+
+class HWSpider(object):
+    def __init__(self):
+        # 起始的请求地址
+        self.start_url = 'http://career.huawei.com/socRecruitment/services/portal3/portalnew/getJobList/page/10'
+        # 起始的翻页页码
+        self.start_page = 1
+        # 翻页条件
+        self.is_running = True
+        # 工作地点列表
+        self.addr_list = []
+        # 部门列表
+        self.dept_list = []
+    def parse_start_url(self):
+        """
+        解析起始的url地址
+        :return:
+        """
+        # 条件循环模拟翻页
+        while self.is_running:
+            # 构造请求参数
+            params = {
+                'curPage': str(self.start_page),
+                'pageSize': '10',
+                'searchType': '1',
+                'orderBy': 'P_COUNT_DESC',
+            }
+            url = self.start_url + '/' + str(self.start_page)
+            headers = {
+                'user-agent': random.choice(USER_AGENT_LIST)
+            }
+            response = session.get(url=url, headers=headers, params=params).json()
+            """调用解析响应方法"""
+            self.parse_response_json(response)
+            """翻页递增"""
+            self.start_page += 1
+            """翻页终止条件"""
+            if self.start_page == 20:
+                self.is_running = False
+    def parse_response_json(self, response):
+        """
+        解析响应
+        :param response:
+        :return:
+        """
+        # 获取岗位信息大列表
+        json_data = response['result']
+        # 判断结果是否有数据
+        if json_data is None:
+            # 没有数据，设置循环条件为False
+            self.is_running = False
+        # 反之，开始提取数据
+        else:
+            # 循环遍历，取出列表中的每一个岗位字典
+            # 通过key取value值的方法进行采集数据
+            for data in json_data:
+                # 工作地点
+                JobArea = data['jobArea']
+                # 往地址大列表中添加数据
+                self.addr_list.append(JobArea)
+                # 部门名称
+                DeptName = data['deptName']
+                # 往工作属性大列表中添加数据
+                self.dept_list.append(DeptName)
+                # 岗位名称
+                Jobname = data['jobname']
+                # 岗位职责
+                Responsibility = data['mainBusiness']
+                # 发布时间
+                LastUpdateTime = data['creationDate']
+                # 构造保存excel所需要的格式字典
+                data_dict = {
+                    # 该字典的key值与创建工作簿的sheet表的名称所关联
+                    '岗位详情': [Jobname, JobArea, DeptName, Responsibility, LastUpdateTime]
+                }
+                """调用保存excel表格方法，数据字典作为参数"""
+                self.save_excel(data_dict)
+                # 提示输出
+                print(f"第{self.start_page}页--岗位{Jobname}----采集完成----logging！！！")
+    def save_excel(self, data_dict):
+        """
+        保存excel
+        :param data_dict: 数据字典
+        :return:
+        """
+        # 判断保存到当我文件目录的路径是否存在
+        os_path_1 = os.getcwd() + '/excel/'
+        if not os.path.exists(os_path_1):
+            # 不存在，即创建这个目录，即创建”数据“这个文件夹
+            os.mkdir(os_path_1)
+        # 判断将数据保存到表格的这个表格是否存在，不存在，创建表格，写入表头
+        os_path = os_path_1 + '华为招聘数据.xls'
+        if not os.path.exists(os_path):
+            # 创建新的workbook（其实就是创建新的excel）
+            workbook = xlwt.Workbook(encoding='utf-8')
+            # 创建新的sheet表
+            worksheet1 = workbook.add_sheet("岗位详情", cell_overwrite_ok=True)
+            excel_data_1 = ('岗位名称', '工作地点', '部门名称', '岗位职责', '发布时间')
+            for i in range(0, len(excel_data_1)):
+                worksheet1.col(i).width = 2560 * 3
+                #               行，列，  内容，            样式
+                worksheet1.write(0, i, excel_data_1[i])
+            workbook.save(os_path)
+        # 判断工作表是否存在
+        # 存在，开始往表格中添加数据(写入数据)
+        if os.path.exists(os_path):
+            # 打开工作薄
+            workbook = xlrd.open_workbook(os_path)
+            # 获取工作薄中所有表的个数
+            sheets = workbook.sheet_names()
+            for i in range(len(sheets)):
+                for name in data_dict.keys():
+                    worksheet = workbook.sheet_by_name(sheets[i])
+                    # 获取工作薄中所有表中的表名与数据名对比
+                    if worksheet.name == name:
+                        # 获取表中已存在的行数
+                        rows_old = worksheet.nrows
+                        # 将xlrd对象拷贝转化为xlwt对象
+                        new_workbook = copy(workbook)
+                        # 获取转化后的工作薄中的第i张表
+                        new_worksheet = new_workbook.get_sheet(i)
+                        for num in range(0, len(data_dict[name])):
+                            new_worksheet.write(rows_old, num, data_dict[name][num])
+                        new_workbook.save(os_path)
+    def run(self):
+        """
+        启动运行
+        :return:
+        """
+        self.parse_start_url()
---
+++ index.py
@@ -0,0 +1,6 @@
+from HWSpider import HWSpider
+
+# 创建该类的对象
+h = HWSpider()
+# 通过实例方法，进行调用
+h.run()
